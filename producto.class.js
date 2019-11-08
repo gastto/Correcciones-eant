6bbd5cdb31fb8862aaacf837c4c402924904ff9f
Gastón Zucconi <you@example.com>
2019-11-07 21:44:53 -0300

    Arreglando sincronizacion + seteo inicial de Google Sign-In

--- producto.class.js
+++ producto.class.js
@@ -1,12 +1,19 @@
 class Producto {
   //Constructor
-  constructor(n, s, p, i, d){
+  constructor(id, n, s, p, i, d=true){
     //Atributos
+    this.ID = id
     this.nombre = n
     this.stock = s
     this.precio = p
     this.imagen = i
     this.disponible = d //<-- Por default asigna "true"
+    this.vDOM = document.createElement("article")
+    
+    this.state = {
+      anexado : false, 
+      version : 0
+    }
   }
 
   //Propiedades Lectura/Escritura (getters & setters)
@@ -15,81 +22,152 @@ class Producto {
   }
 
   set Precio(value){
-
     if( isNaN(value) != true ){
       this.precio = value
     } else {
       console.error("ERROR: Valor ingresado NO válido")
     }
-
   }
 
   set Disponible(value){
-
-    // if( value == this.disponible ){
-    //   alert("La disponibilidad ya está en: " + value)
-    //   return
-    // }
-
-    // let estado = value ? "habilitar" : "deshabilitar"
-
-    // if( confirm(`Desea ${estado} el producto "${this.nombre}"`) ){
-    //   this.disponible = value
-    // }
-
+    let accion = value ? "habilitar" : "deshabilitar"
+    if( confirm(`Desea ${accion} el producto "${this.nombre}"`) )
+      this.disponible = value
+      
+      this.sincronizar()
   }
 
+  //Metodos de Instancia //render de react
+  Mostrar(selector){
+      let estilo = this.disponible ? "bg-light text-dark" : "bg-dark text-light"
+        // Manipulacion de Estructura
+        this.vDOM.classList.add("col-lg-4", "col-md-6", "mb-4", "producto")// ← <article class="col-lg-4 col-md-6 mb-4s producto"></article>
 
-  //Metodos de Instancia
-  Mostrar(selector){ //← Ej: "#productos-destacados"
-    let ficha = document.createElement("article") // ← <article></article>
-    let Disponible = (this.stock > 0) ? "bg-light text-dark" : "bg-dark text-light"
+        this.vDOM.id = `prod-${this.ID}`
 
-        ficha.classList.add("col-lg-4", "col-md-6", "mb-4", "producto")// ← <article class="col-lg-4 col-md-6 mb-4s producto"></article>
+        // Manipulacion de Contenido
+        this.vDOM.innerHTML = `<div class="card h-100 ${estilo}">
+                                <a href="#">
+                                  <img class="card-img-top img-fluid" src="${this.imagen}" alt="${this.nombre}">
+                                </a>
+                                <div class="card-body">
+                                  <h4 class="card-title">
+                                    <a href="#">${this.nombre}</a>
+                                  </h4>
+                                  <button class="btn btn-warning m-0 btn-precio">${this.Precio}</button>
+                                  <button class="btn btn-disponible ${this.disponible ? "btn-danger" : "btn-success" }">${ this.disponible ? "Desactivar" : "Activar" }</button>
+                                  <button class="btn btn-primary btn-descuento">Aplicar Descuento</button>
 
-        ficha.innerHTML = `<div class="card h-100 ${Disponible}">
-                            <a href="#">
-                              <img class="card-img-top" src="${this.imagen}" alt="${this.nombre}">
-                            </a>
-                            <div class="card-body">
-                              <h4 class="card-title">
-                                <a href="#">${this.nombre}</a>
-                              </h4>
-                              <h5 class="btn btn-warning">${this.Precio}</h5>
-                              <p class="card-text">${this.stock} unid.</p>
-                            </div>
-                          </div>`
+                                  <p class="card-text">${this.stock} unid.</p>
+                                </div>
+                              </div>`
 
-    document.querySelector(selector).appendChild( ficha )
+      // Manipulacion de Comportamiento
+      this.vDOM.querySelector(".btn-disponible").onclick = (e) => {
+        
+        this.Disponible = !this.disponible
+        
+        this.Mostrar()
+        
+        console.log( e.target )
 
-    console.log( ficha )
-  }
+        this.sincronizar()
+      }
+      
+      this.vDOM.querySelector(".btn-precio").onclick = () => {
+        
+        this.Precio = prompt("Ingrese nuevo precio:")
+        
+        this.Mostrar()
+      
+      }
 
-  aplicarDescuento(valor){
-    let importe = (this.precio * valor) / 100
+      // this.vDOM.querySelector(".btn-descuento").onclick = () => {
+        //   let valor = prompt(`Indique el % de descuento para ${this.nombre}`)
+        //   this.aplicarDescuento(valor)
+        //   this.Mostrar()
+        // }
+        
+        this.vDOM.querySelector(".btn-descuento").onclick = this.aplicarDescuento.bind( this )
+        
+        
+        this.vDOM.querySelector("img").onclick = () => {
+          this.imagen = prompt('Ingrese la URL de una nueva imagen:')
+          this.Mostrar()
+          this.sincronizar()
+        }
 
-    this.precio = this.precio - importe
-  }
+        
+        // Anexarlo en la interfaz
+        if( !this.state.anexado ){
+          document.querySelector(selector).appendChild( this.vDOM )
+          this.state.anexado = true
+        }
 
+        // this.sincronizar()
+      }
+      
+      aplicarDescuento(valor = false){
+        
+        // valor = valor == null ? prompt(`Indique el % de descuento para ${this.nombre}`) : valor
+        
+        valor = isNaN(valor) ? prompt(`Indique el % de descuento para ${this.nombre}`) : valor
+
+        let importe = (this.precio * valor) / 100
+        this.precio = this.precio - importe
+
+        this.Mostrar()
+        this.sincronizar()
+      }
+
+      sincronizar(){
+
+        let storage = JSON.parse(localStorage.getItem("PRODUCTOS") )// <-- JSON a Object 
+
+        //let foundItem = storage.find(item => item.idProducto == this.ID)
+        let foundIndex = storage.findIndex(item => item.idProducto == this.ID)
+
+        storage[foundIndex].Precio = this.precio
+        storage[foundIndex].Disponible = this.disponible
+        storage[foundIndex].Imagen = this.imagen
+
+
+        // console.log( foundItem )
+        // console.log( foundIndex )
+
+        // storage.forEach(item => {
+
+        //   if( item.idProducto == this.ID ){
+            
+        //     item.Nombre = this.nombre
+        //     item.Stock = this.stock
+        //     item.Precio = this.precio
+        //     item.Disponible = this.disponible
+        //     return
+        //   }
+
+        // })
+
+        
+
+        localStorage.setItem("PRODUCTOS", JSON.stringify(storage)) // <-- Objec a JSON
+      }
+
+      
+      
   //Metodos de Clase (estáticos)
-  static parse(json){ //<-- Ej: '[{"nombre":"Café Torrado","stock":600,"precio":85.65,"disponible":false},{"nombre":"Jugo de Naranja","stock":450,"precio":15.45,"disponible":true}]'
-    //Acá hay que h acer magia para que se conviertan en objetos 'Producto'
-    //let datos = JSON.parse(json) //<-- de JSON a Object
-    //let datos = json //<-- de JSON a Object
-
+  static parse(json){ 
     let datos = (typeof json == "string") ? JSON.parse(json) : json
-
     console.log("Estos son los datos:")
     console.log(datos)
 
     if( datos instanceof Array ){
-
       //1) Recorrer el Array de Object para instanciar objetos Producto y retornarlos
-      return datos.map(item => new Producto(item.Nombre, item.Stock, item.Precio, item.Imagen) ) //2) <-- Instanciar un objeto Producto con los datos de cada Object
+      return datos.map(item => new Producto(item.idProducto,item.Nombre, item.Stock, item.Precio, item.Imagen, item.Disponible) ) //2) <-- Instanciar un objeto Producto con los datos de cada Object
 
     } else if( datos instanceof Object ){
 
-      return new Producto(datos.Nombre, datos.Stock, datos.Precio, datos.Imagen)
+      return new Producto(datos.idProducto, datos.Nombre, datos.Stock, datos.Precio, datos.Imagen, datos.Disponible)
 
     } else {
       console.error("Ya fue... no convierto nada en Producto")
@@ -97,6 +175,12 @@ class Producto {
 
   }
 }
+
+// Javascript modificaciones
+
+// modificacion de estructura
+// modificacion de contenido
+// modificacion de comportamiento
 
 
 
